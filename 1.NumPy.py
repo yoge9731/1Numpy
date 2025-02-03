c0f6a7a94bba3e033b7677d9693ef7f471a8a482
YOGEESH K P <yogeesh793@gmail.com>
2025-02-03 11:59:37 +0530

    Add files via upload

---
+++ 1.NumPy.py
@@ -0,0 +1,19 @@
+# 1
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(type(arr))
+
+# 2
+
+import numpy as yp
+yo = yp.array([5,10,15,20])
+print(yo)
+print(type(yo))
+
+
+
+
